04d11944a696907d51b4f09ecc673079b1618215
rthawk <hawk@rtks.net>
2017-03-21 19:09:12 +0900

    add move layer

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -4,6 +4,7 @@
 #define _QWERTY 0
 #define _NUMBER 1
 #define _SYMBOL 2
+#define _MOVE   3
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
@@ -11,27 +12,35 @@
 
 #define MO_NUML MO(_NUMBER)
 #define MO_SYML MO(_SYMBOL)
+#define MO_MOVE MO(_MOVE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = {
   {KC_TAB,  KC_Q ,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_LCTL, XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN},
   {KC_LSFT, KC_Z,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT},
-  {KC_LCTL, KC_LALT, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,  MO_SYML, MO_NUML, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT}
+  {MO_MOVE, KC_LALT, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,  MO_SYML, MO_NUML, KC_LEFT, KC_DOWN, KC_UP  , KC_RGUI}
 },
 
 [_NUMBER] = {
-  {KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC},
+  {KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_MINS},
   {_______, XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0},
   {_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______},
   {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______},
 },
 
 [_SYMBOL] = {
-  {KC_DEL,  XXXXXXX, KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE},
-  {KC_TILD, XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN},
+  {KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE},
+  {_______, XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN},
   {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,_______, _______,  _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET}
+},
+
+[_MOVE] = {
+  {KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE},
+  {KC_TILD, XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______},
+  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_END , KC_ENT,  KC_BSPC, _______, _______},
+  {_______, _______, _______, _______, KC_BTN1, _______, _______, _______, _______, _______, _______, KC_RGUI}
 },
 
 };
