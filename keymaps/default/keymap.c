48a301b8f4140d6d0c7b1a9d121848a4ed7bff0c
rthawk <hawk@rtks.net>
2017-03-21 16:37:26 +0900

    add default keymap

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -1,10 +1,20 @@
 #include "dirac.h"
 
+// layers
+#define _QWERTY 0
+
+// Fillers to make layering more clear
+#define _______ KC_TRNS
+#define XXXXXXX KC_NO
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[0] = KEYMAP( /* Base */
-  KC_A,  KC_1,  KC_H, \
-    KC_TAB,  KC_SPC   \
-),
+[_QWERTY] = {
+  {KC_TAB,  KC_Q ,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
+  {KC_T,    KC_NO,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN},
+  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
+  {KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,   KC_SPC,  KC_SPC,  KC_SPC,   KC_LEFT, KC_DOWN, KC_UP, KC_RGHT}
+}
+
 };
 
 const uint16_t PROGMEM fn_actions[] = {
