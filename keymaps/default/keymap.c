fd56b7d7f5e7d6eed18382b3224bae4888f5a98b
rthawk <hawk@rtks.net>
2017-03-21 18:36:34 +0900

    add symbol layer

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -3,19 +3,21 @@
 // layers
 #define _QWERTY 0
 #define _NUMBER 1
+#define _SYMBOL 2
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
 #define MO_NUML MO(_NUMBER)
+#define MO_SYML MO(_SYMBOL)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = {
   {KC_TAB,  KC_Q ,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_LCTL, XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN},
   {KC_LSFT, KC_Z,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT},
-  {KC_LCTL, KC_LALT, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,  KC_SPC,  MO_NUML, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT}
+  {KC_LCTL, KC_LALT, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,  MO_SYML, MO_NUML, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT}
 },
 
 [_NUMBER] = {
@@ -25,6 +27,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______},
 },
 
+[_SYMBOL] = {
+  {KC_DEL,  XXXXXXX, KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE},
+  {KC_TILD, XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN},
+  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,_______, _______,  _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET}
+},
 
 };
 
