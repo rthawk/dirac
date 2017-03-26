0c726c4e16f0e4223f1c9e86d1b9deb650b009ee
rthawk <hawk@rtks.net>
2017-03-26 16:02:09 +0900

    fix symbol layer

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -14,7 +14,7 @@
 #define MO_NUML MO(_NUMBER)
 #define MO_SYML MO(_SYMBOL)
 #define MO_MOVE MO(_MOVE)
-#define KC_RSPC SFT_T(KC_SPC)
+#define KC_SSPC SFT_T(KC_SPC)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_ESC , KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    ALT_T(KC_BSPC)},
   {KC_LCTL, XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    CTL_T(KC_SCLN)},
   {KC_LSFT, KC_Z,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  SFT_T(KC_SLSH)},
-  {MO_MOVE, KC_LALT, KC_LGUI, KC_LALT, MO_SYML, KC_SPC,  KC_RSPC, MO_NUML, KC_RALT, KC_RCTL, TG(_QGMLWY), KC_RGUI}
+  {MO_MOVE, KC_LALT, KC_LGUI, KC_LALT, MO_SYML, KC_SPC,  KC_SSPC, MO_NUML, KC_RALT, KC_RCTL, TG(_QGMLWY), KC_RGUI}
 },
 
 [_QGMLWY] = {
@@ -40,14 +40,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 },
 
 [_SYMBOL] = {
-  {KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, KC_BSLS},
+  {KC_GRV,  KC_EXLM, KC_DQT,  KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, KC_BSLS},
   {_______, XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN},
-  {_______, _______, _______, _______, _______, KC_LBRC, KC_BSLS, KC_RBRC, _______, KC_QUOT, KC_GRV,  _______},
+  {_______, _______, _______, KC_DQT , _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
 [_MOVE] = {
-  {KC_TAB,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_PGUP, KC_TAB,  KC_LCBR, KC_HOME, KC_PIPE},
+  {KC_TAB,  KC_EXLM, KC_DQT,  KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_PGUP, KC_TAB,  KC_LCBR, KC_HOME, KC_PIPE},
   {_______, XXXXXXX, KC_EXLM, KC_AT,   KC_PGDN, KC_DLR,  KC_PERC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT},
   {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_END , KC_ENT,  KC_BSPC, _______, _______},
   {_______, _______, _______, _______, KC_BTN1, KC_BTN1, _______, _______, _______, _______, _______, _______}
